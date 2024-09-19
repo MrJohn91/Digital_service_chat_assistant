343438b9dbdfe51ff472ce8faba82bb2629922cd
Mr J <nfluncvjohn@gmail.com>
2024-09-19 21:47:49 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,9 +13,7 @@ def load_chatbot():
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     
     llm = HuggingFaceHub(
-        repo_id="mistralai/Mistral-7B-Instruct-v0.3",
-        huggingfacehub_api_token=huggingfacehub_api_token
-    )
+        repo_id="mistralai/Mistral-7B-Instruct-v0.3")
     # Loading FAQ data
     with open("spotify_faq_data.json") as f:
         faq_data = json.load(f)

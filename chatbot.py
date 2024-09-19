62eb3925aa73e4f3ed2d4a6219e9623feae4f698
Mr J <nfluncvjohn@gmail.com>
2024-09-19 20:01:16 +0200

    Update chatbot.py
    code update

--- chatbot.py
+++ chatbot.py
@@ -1,32 +1,38 @@
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import HuggingFaceEndpoint
 from langchain.vectorstores import FAISS
-from langchain.llms import HuggingFaceHub
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
+from langchain.llms import HuggingFaceHub
 import json
 import os
 
 def load_chatbot():
-    huggingfacehub_api_token = os.getenv("HUGGING_FACE_TOKEN")
+    # embedding model
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
     
-  llm = HuggingFaceHub(
+    huggingfacehub_api_token = os.getenv("HUGGING_FACE_TOKEN")
+
+    # Initialize Hugging Face Hub with the token
+    llm = HuggingFaceHub(
         repo_id="mistralai/Mistral-7B-Instruct-v0.3",
         huggingfacehub_api_token=huggingfacehub_api_token
     )
+
     
     with open("spotify_faq_data.json") as f:
         faq_data = json.load(f)
 
+    
     faq_text = [f"{faq['question']} {faq['answer']}" for faq in faq_data]
 
+    
     vector_store = FAISS.from_texts(faq_text, embedding_model)
 
+    
     memory = ConversationBufferMemory(memory_key="chat_history")
 
-
+    
     qa_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vector_store.as_retriever(),
@@ -39,4 +45,3 @@ def load_chatbot():
 def ask_question(qa_chain, query):
     result = qa_chain({"question": query})
     return result["answer"]
-

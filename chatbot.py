91abcd1bc37c7977b9e0827562a3bf2896ca59e4
Mr J <nfluncvjohn@gmail.com>
2024-09-19 21:52:04 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -9,21 +9,18 @@ import os
 def load_chatbot():
     huggingfacehub_api_token = os.getenv("HUGGING_FACE_TOKEN")
     
-    # Embedding model
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     
-  llm = HuggingFaceHub(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.3", 
-    huggingfacehub_api_token=huggingfacehub_api_token
-)
+    llm = HuggingFaceHub(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.3", 
+        huggingfacehub_api_token=huggingfacehub_api_token
+    )
 
-    # Loading FAQ data
     with open("spotify_faq_data.json") as f:
         faq_data = json.load(f)
 
     faq_text = [f"{faq['question']} {faq['answer']}" for faq in faq_data]
 
-    # VectorStore and Memory
     vector_store = FAISS.from_texts(faq_text, embedding_model)
     memory = ConversationBufferMemory(memory_key="chat_history")
 
